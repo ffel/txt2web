e1f68c22faf0294608cab53bf27e4c7b1a543548
Chris van Uffelen <vanuffelen@gmail.com>
2015-06-23 21:12:04 +0200

    uses a table test instead of an example.
    table tests show which tests fail.  Examples fail if one part fails.

--- refs_test.go
+++ refs_test.go
@@ -1,56 +1,72 @@
 package txt2web
 
-// ref before id!!
+import "testing"
 
-var tests = []string{
-	`
+const in1 = `
 # section 1
 
 # section 2
 
 see [section 1](#section-1)
-	`, `
+`
+
+const out1 = `section 1 {#/path/section-1}
+=========
+
+section 2 {#/path/section-2}
+=========
+
+see [section 1](#/path/section-1)
+`
+
+const in2 = `
 # section 1
 
 see [section 2](#section-2)
 
 # section 2
-	`, `
+`
+
+const out2 = `section 1 {#/path/section-1}
+=========
+
+see [section 2](#/path/section-2)
+
+section 2 {#/path/section-2}
+=========
+`
+
+const in3 = `
 # section 1
 
 see [section 2](#foo)
 
 # section 2 {#foo}
-	`,
-}
+`
 
-func ExampleReferences() {
-	markdownTerm(References(setFile(contentGen(tests...), "path/file.txt")))
+const out3 = `section 1 {#/path/section-1}
+=========
 
-	// output:
-	// section 1 {#/path/section-1}
-	// =========
-	//
-	// section 2 {#/path/section-2}
-	// =========
-	//
-	// see [section 1](#/path/section-1)
-	// ---
-	// section 1 {#/path/section-1}
-	// =========
-	//
-	// see [section 2](#/path/section-2)
-	//
-	// section 2 {#/path/section-2}
-	// =========
-	// ---
-	// section 1 {#/path/section-1}
-	// =========
-	//
-	// see [section 2](#/path/foo)
-	//
-	// section 2 {#/path/foo}
-	// =========
-	// ---
+see [section 2](#/path/foo)
 
+section 2 {#/path/foo}
+=========
+`
+
+func TestReferences(t *testing.T) {
+	inout := []struct{ in, out string }{
+		{in1, out1},
+		{in2, out2},
+		{in3, out3},
+	}
+
+	for _, tt := range inout {
+		c := markdownChan(References(setFile(contentGen(tt.in), "path/file.txt")))
+
+		got := string(<-c)
+
+		if got != tt.out {
+			t.Errorf("%v gave unexpexted %v, not %v", tt.in, got, tt.out)
+		}
+	}
 }
