aceca1475b489362f0841f81be7989b13763ac32
Chris van Uffelen <vanuffelen@gmail.com>
2015-06-25 06:34:36 +0200

    fix test

--- htmlwriter_test.go
+++ htmlwriter_test.go
@@ -14,5 +14,5 @@ func ExampleWriteHtml() {
 	// -- fileb.txt:
 	// <h1 id="drie-pellentesque-lobortis-lacus">Drie Pellentesque lobortis lacus</h1>
 	// <p>Condimentum rutrum enim blandit. Sed vitae luctus libero. Aliquam erat volutpat. Morbi accumsan sem sodales lorem congue placerat. Nam auctor sapien id libero vulputate, non sodales nibh tempus. Donec sagittis consectetur magna sit amet vehicula. Vivamus sit amet dui eget urna vestibulum gravida. Quisque et mauris vehicula, maximus nisi luctus, pellentesque dui.</p>
-	// <p>Refer to <a href="/#twee">two</a>.</p>
+	// <p>Refer to <a href="#/twee-morbi-finibus-rutrum-condimentum">two</a>.</p>
 }
