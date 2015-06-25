1e23a6802c13c7865c30c57d42a4f6abfbd6cdf4
Chris van Uffelen <vanuffelen@gmail.com>
2015-06-25 06:49:56 +0200

    most explaining functions first

--- txt2web.go
+++ txt2web.go
@@ -7,10 +7,29 @@ import (
 	"github.com/ffel/piperunner"
 )
 
-// init starts the pool of pipe runners which is the worker pool of
-// pandoc processes
-func init() {
-	piperunner.StartPool()
+// Convert does the complete txt2web conversion
+func Convert(txtroot, destination string) <-chan HtmlFile {
+
+	var filenamec <-chan string
+
+	// find file names, ignore possible clashes if destination is a sub map
+	filenamec = TxtFiles(txtroot, destination)
+
+	var chunkc <-chan Chunk
+
+	// read entire files in chunks
+	chunkc = Generate(filenamec)
+	// replace anchors for angular routes
+	chunkc = References(chunkc)
+	// split chunks into one section per chunk
+	chunkc = Split(chunkc)
+
+	var htmlc <-chan HtmlFile
+
+	// send file name and contents
+	htmlc = WriteHtml(chunkc)
+
+	return htmlc
 }
 
 // Chunk is the basis data object for one #-section
@@ -47,27 +66,8 @@ type HtmlFile struct {
 	Path     string
 }
 
-// Convert does the complete txt2web conversion
-func Convert(txtroot, destination string) <-chan HtmlFile {
-
-	var filenamec <-chan string
-
-	// find file names, ignore possible clashes if destination is a sub map
-	filenamec = TxtFiles(txtroot, destination)
-
-	var chunkc <-chan Chunk
-
-	// read entire files in chunks
-	chunkc = Generate(filenamec)
-	// replace anchors for angular routes
-	chunkc = References(chunkc)
-	// split chunks into one section per chunk
-	chunkc = Split(chunkc)
-
-	var htmlc <-chan HtmlFile
-
-	// send file name and contents
-	htmlc = WriteHtml(chunkc)
-
-	return htmlc
+// init starts the pool of pipe runners which is the worker pool of
+// pandoc processes
+func init() {
+	piperunner.StartPool()
 }
